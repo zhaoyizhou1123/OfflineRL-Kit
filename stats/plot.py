0d123481f756b98e274a302772ea2d27a1407b9c
Zhaoyi Zhou <zhouzhao20@mails.tsinghua.edu.cn>
2023-09-26 11:15:23 +0800

    Updated plot

--- stats/plot.py
+++ stats/plot.py
@@ -7,16 +7,27 @@ from offlinerlkit.env.linearq import Linearq
 env = Linearq(size_param=4)
 num_states = env.state_space_size
 
+s_arr = np.arange(num_states)
+
 # a = 0
 qf0 = []
 for s in range(num_states):
     qf = env._get_q(s,0)
     qf0.append(qf)
-qf0 = np.array(qf)
-s_arr = np.arange(num_states)
+qf0 = np.array(qf0)
 
-plt.plot(s_arr, qf0)
-plt.save("linearq.png")
+qf1 = []
+for s in range(num_states):
+    qf = env._get_q(s,1)
+    qf1.append(qf)
+qf1 = np.array(qf1)
+
+plt.plot(s_arr, qf0, color='r', marker='o', label="$Q(s,a=0)$")
+plt.plot(s_arr, qf1, color='b', marker='s', label="$Q(s,a=1)$")
+plt.xlabel("state")
+plt.ylabel("$Q$-function")
+plt.legend()
+plt.savefig("linearq.png")
 
 
 
